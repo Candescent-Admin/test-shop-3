f38bf7167756c8a27b19fc25237c305f76f16841
Candescent-Admin <info@candescent.ca>
2016-11-23 02:13:53 -0500

    generated

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+
+  config.api_key = "d5f801a888b8ab2c57501b7b00393d52"
+  config.secret = "45a10e69a105c0f45c6184c8eb3365fb"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
